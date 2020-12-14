92511d8300aa3c916962ea6ba32698e803c97570
Robert Newton <robert.newton11@gmail.com>
2020-12-13 17:24:14 -0800

    doc: pubsub

--- infrastructure/pubsub/pubsub.go
+++ infrastructure/pubsub/pubsub.go
@@ -1,27 +1,36 @@
+// Package pubsub is a very simple channel-based event system.
+// TODO - consider using a messaging backend instead of in-memory channels?
 package pubsub
 
 import (
 	"sync"
 )
 
+// Message is a wrapper around an event that has occurred with a name.
+// e.g: User/Created, { "name": "New user", "email": "email@example.com", ... }
 type Message struct {
 	Topic string
 	Data  interface{}
 }
 
+// EventChan is a channel for sending event messages
 type EventChan chan Message
 
+// EventChans is a slice of channels
 type EventChans []EventChan
 
+// EventBus handles the subscribers for the different events
 type EventBus struct {
 	Subscribers map[string]EventChans
 	lock        sync.RWMutex
 }
 
 var (
+	// Instance ensures singleton access to the event bus
 	Instance = &EventBus{Subscribers: map[string]EventChans{}}
 )
 
+// Publish will push a message to all subscribers for a topic
 func (b *EventBus) Publish(topic string, event interface{}) {
 	b.lock.RLock()
 
@@ -37,6 +46,7 @@ func (b *EventBus) Publish(topic string, event interface{}) {
 	b.lock.RUnlock()
 }
 
+// Subscribe will add the specified channel to the list of subscribers for a topic.
 func (b *EventBus) Subscribe(topic string, ch EventChan) {
 	b.lock.Lock()
 
