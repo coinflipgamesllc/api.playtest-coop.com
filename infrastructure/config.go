f6d4a9b05f44d115b65d8c0bee75700af08e413e
Robert Newton <robert.newton11@gmail.com>
2020-12-17 10:28:12 -0800

    fix: only pull from .env in development

--- infrastructure/config.go
+++ infrastructure/config.go
@@ -7,12 +7,17 @@ import (
 )
 
 func init() {
-	viper.SetConfigType("dotenv")
-	viper.SetConfigName(".env")
-	viper.AddConfigPath(".")
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Failed to parse config file: %v\n", err)
+	// Only pull from .env in development
+	environment := viper.GetString("ENVIRONMENT")
+	if environment == "development" || environment == "" {
+		viper.SetConfigType("dotenv")
+		viper.SetConfigName(".env")
+		viper.AddConfigPath(".")
+
+		if err := viper.ReadInConfig(); err != nil {
+			log.Fatalf("Failed to parse config file: %v\n", err)
+		}
 	}
 }
